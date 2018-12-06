9128220d289fb33b9266c71dbc442dbbddafecad
Mashkour <azizmashkour@gmail.com>
2018-12-06 12:58:58 +0100

    fix: separate app state into config file

---
+++ src/config/AppState.js
@@ -0,0 +1,21 @@
+export const AppState = {
+  color: '#000',
+  node: {
+    A: 'A',
+    B: 'B',
+    C: 'C',
+  },
+  triangleA: [
+    { x: 150, y: 250 },
+    { x: 300, y: 300 },
+    { x: 200, y: 400 },
+  ],
+  triangleB: [
+    { x: 550, y: 650 },
+    { x: 500, y: 500 },
+    { x: 400, y: 600 },
+  ],
+  linePoints: [680, 50, 200, 600],
+};
+
+export default AppState;
