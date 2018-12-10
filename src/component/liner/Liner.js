7fc433c0bf17f701bca407996b19b4aec2dc74b2
Mashkour <azizmashkour@gmail.com>
2018-12-10 16:58:45 +0100

    fix: refactoring and updating line axes coordinates

--- src/component/liner/Liner.js
+++ src/component/liner/Liner.js
@@ -10,9 +10,59 @@ const Liner = ({
   color,
   handleDragMove,
   linePoints,
+  lineAxeOne,
+  lineAxeTwo,
+  lineAxeThree,
 }) => (
   <Layer>
-    <Line points={[linePoints[0], linePoints[1], linePoints[2], linePoints[3]]} stroke="#ced4da" strokeWidth={5} />
+    <Line
+      points={
+        [
+          linePoints[0],
+          linePoints[1],
+          linePoints[2],
+          linePoints[3],
+        ]
+      }
+      stroke="#ced4da"
+      strokeWidth={5}
+    />
+    <Line
+      points={
+        [
+          lineAxeOne[0],
+          lineAxeOne[1],
+          lineAxeOne[2],
+          lineAxeOne[3],
+        ]
+      }
+      stroke="#1DE9B6"
+      strokeWidth={1}
+    />
+    <Line
+      points={
+        [
+          lineAxeTwo[0],
+          lineAxeTwo[1],
+          lineAxeTwo[2],
+          lineAxeTwo[3],
+        ]
+      }
+      stroke="#1DE9B6"
+      strokeWidth={1}
+    />
+    <Line
+      points={
+        [
+          lineAxeThree[0],
+          lineAxeThree[1],
+          lineAxeThree[2],
+          lineAxeThree[3],
+        ]
+      }
+      stroke="#1DE9B6"
+      strokeWidth={1}
+    />
     <Circle
       x={linePoints[0]}
       y={linePoints[1]}
@@ -40,6 +90,9 @@ Liner.propTypes = {
   color: PropTypes.string.isRequired,
   handleDragMove: PropTypes.func.isRequired,
   linePoints: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+  lineAxeOne: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+  lineAxeTwo: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+  lineAxeThree: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
 };
 
 export default Liner;
