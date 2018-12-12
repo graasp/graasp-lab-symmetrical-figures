05b9e438429d67bcb8bc9edf74971a86e4574e1f
Mashkour <azizmashkour@gmail.com>
2018-12-12 11:10:26 +0100

    feat: create different component for dynamic form choice

---
+++ src/component/forms/Forms.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  Col,
+  Input,
+  Row,
+} from 'reactstrap';
+import Triangle from './Triangle.svg';
+import Rectangle from './Rectangle.svg';
+import Polygon from './Polygon.svg';
+import './Forms.css';
+
+const Forms = () => (
+  <div className="main-container">
+    <h2 className="description-choice">
+      Choisissez une forme
+    </h2>
+    <Row>
+      <Col md={4}>
+        <img
+          src={Triangle}
+          alt="Triangle"
+          className="triangle-figure"
+        />
+      </Col>
+      <Col md={4}>
+        <img
+          src={Rectangle}
+          alt="Rectangle"
+          className="rectangle-figure"
+        />
+      </Col>
+      <Col md={4}>
+        <img
+          src={Polygon}
+          alt="Polygon"
+          className="polygon-figure"
+        />
+      </Col>
+    </Row>
+    <Row className="forms-choice">
+      <Col md={4}>
+        <Input
+          type="radio"
+          name="triangle-choice"
+          className="form-checker"
+        />
+      </Col>
+      <Col md={4}>
+        <Input
+          type="radio"
+          name="square-choice"
+          className="form-checker"
+        />
+      </Col>
+      <Col md={4}>
+        <Input
+          type="radio"
+          name="polygon-choice"
+          className="form-checker"
+        />
+      </Col>
+    </Row>
+  </div>
+);
+
+export default Forms;
