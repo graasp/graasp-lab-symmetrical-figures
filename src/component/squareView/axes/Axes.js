6e1bd5d4e040782751f1d4a47e2ca4e3b3227d17
Mashkour <azizmashkour@gmail.com>
2018-12-13 16:28:34 +0100

    feat: create square axes component and validate it props

---
+++ src/component/squareView/axes/Axes.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Circle,
+  Layer,
+  Line,
+} from 'react-konva';
+
+const Axes = ({
+  CIRCLE_RADIUS,
+  CIRCLE_X,
+  CIRCLE_Y,
+  IDENTIC_PATH_2,
+  IDENTIC_PATH_3,
+  IDENTIC_PATH_4,
+  IDENTIC_PATH_5,
+  IDENTIC_PATH_6,
+  blackStroke,
+  blueStroke,
+  strokeWidth,
+  shadowBlur,
+}) => (
+  <Layer>
+    <Circle
+      x={CIRCLE_X}
+      y={CIRCLE_Y}
+      radius={CIRCLE_RADIUS}
+      fill={blackStroke}
+      strokeWidth={strokeWidth}
+      shadowBlur={shadowBlur}
+    />
+    <Line
+      points={
+        [
+          IDENTIC_PATH_3,
+          IDENTIC_PATH_3,
+          IDENTIC_PATH_5,
+          IDENTIC_PATH_4,
+        ]
+      }
+      stroke={blueStroke}
+      strokeWidth={strokeWidth}
+    />
+    <Line
+      points={
+        [
+          IDENTIC_PATH_5,
+          IDENTIC_PATH_3,
+          IDENTIC_PATH_3,
+          IDENTIC_PATH_4,
+        ]
+      }
+      stroke={blueStroke}
+      strokeWidth={strokeWidth}
+    />
+    <Line
+      points={
+        [
+          IDENTIC_PATH_2,
+          IDENTIC_PATH_3,
+          IDENTIC_PATH_6,
+          IDENTIC_PATH_4,
+        ]
+      }
+      stroke={blackStroke}
+      strokeWidth={strokeWidth}
+    />
+    <Line
+      points={
+        [
+          IDENTIC_PATH_2,
+          IDENTIC_PATH_4,
+          IDENTIC_PATH_6,
+          IDENTIC_PATH_3,
+        ]
+      }
+      stroke={blackStroke}
+      strokeWidth={strokeWidth}
+    />
+  </Layer>
+);
+
+Axes.propTypes = {
+  CIRCLE_RADIUS: PropTypes.number.isRequired,
+  CIRCLE_X: PropTypes.number.isRequired,
+  CIRCLE_Y: PropTypes.number.isRequired,
+  blackStroke: PropTypes.string.isRequired,
+  blueStroke: PropTypes.string.isRequired,
+  strokeWidth: PropTypes.number.isRequired,
+  IDENTIC_PATH_2: PropTypes.number.isRequired,
+  IDENTIC_PATH_3: PropTypes.number.isRequired,
+  IDENTIC_PATH_4: PropTypes.number.isRequired,
+  IDENTIC_PATH_5: PropTypes.number.isRequired,
+  IDENTIC_PATH_6: PropTypes.number.isRequired,
+  shadowBlur: PropTypes.number.isRequired,
+};
+
+export default Axes;
