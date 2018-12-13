3264f2fee5a7646d46c7a82272c0356dec761352
Mashkour <azizmashkour@gmail.com>
2018-12-13 11:07:27 +0100

    fix: use class component instead of writing as pure function

--- src/component/triangleView/TriangleView.js
+++ src/component/triangleView/TriangleView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Stage } from 'react-konva';
 import Triangle from '../triangle/Triangle';
@@ -6,200 +6,200 @@ import Points from '../points/Points';
 import Liner from '../liner/Liner';
 import Axes from '../axes/Axes';
 import MidPoint from '../axes/MidPoint';
+import { AppState } from '../../config/AppState';
 
-const TriangleView = ({
-  axePointsOne,
-  axePointsTwo,
-  axeStroke,
-  axeStrokeWidth,
-  axePointsThree,
-  circlePoints,
-  circleRadius,
-  color,
-  handleDragMove,
-  handleDragMoveOne,
-  height,
-  lineAxeOne,
-  lineAxeTwo,
-  lineAxeThree,
-  linePoints,
-  shapeStroke,
-  lineStroke,
-  lineStrokeWidth,
-  midPointStroke,
-  midPointStrokeWidth,
-  midPointShadowBlur,
-  midPointRadius,
-  nodeA,
-  nodeB,
-  pointSize,
-  showPoints,
-  toggleLine,
-  triangleA,
-  triangleB,
-  triangleOpacity,
-  triangleStroke,
-  triangleShadowBlur,
-  triangleStrokeWidth,
-  width,
-}) => (
-  <div className="triangle-content">
-    { toggleLine ? (
-      <Stage width={width} height={height}>
-        <MidPoint
-          circlePoints={circlePoints}
-          color={color}
-          shadowBlur={midPointShadowBlur}
-          stroke={midPointStroke}
-          strokeWidth={midPointStrokeWidth}
-          radius={midPointRadius}
-        />
-        <Axes
-          axePointsOne={axePointsOne}
-          axePointsTwo={axePointsTwo}
-          axePointsThree={axePointsThree}
-          stroke={axeStroke}
-          strokeWidth={axeStrokeWidth}
-        />
-      </Stage>
-    )
-      : (
+class TriangleView extends Component {
+  static propTypes = {
+    showPoints: PropTypes.bool.isRequired,
+    toggleLine: PropTypes.bool.isRequired,
+    nodeB: PropTypes.shape({
+      A: PropTypes.string.isRequired,
+      B: PropTypes.string.isRequired,
+      C: PropTypes.string.isRequired,
+    }).isRequired,
+  };
+
+  state = AppState;
+
+  handleLineChange = (newLinePoints) => {
+    this.setState({
+      linePoints: newLinePoints,
+    });
+  }
+
+  handleTriangleChange = (newTriangle) => {
+    this.setState({
+      triangleA: newTriangle,
+    });
+  }
+
+  handleDragMoveOne = (e) => {
+    const { linePoints } = this.state;
+    const newLinePoints = [...linePoints];
+    newLinePoints[2] = e.target.x();
+    newLinePoints[3] = e.target.y();
+    this.handleLineChange(newLinePoints);
+  }
+
+  handleDragMove = (e) => {
+    const { linePoints, triangleA } = this.state;
+    const newLinePoints = [...linePoints];
+    const newTriangleAPoints = [...triangleA];
+    newLinePoints[0] = e.target.x();
+    newLinePoints[1] = e.target.y();
+    const newTriangle = newTriangleAPoints.map(({ x, y }) => ({
+      x: x + e.target.x(),
+      y: y + e.target.y(),
+    }));
+    this.handleLineChange(newLinePoints);
+    this.handleTriangleChange(newTriangle);
+  }
+
+  render() {
+    const {
+      axePointsOne,
+      axePointsTwo,
+      axePointsThree,
+      axeStroke,
+      axeStrokeWidth,
+      circlePoints,
+      circleRadius,
+      color,
+      height,
+      linePoints,
+      lineAxeOne,
+      lineAxeTwo,
+      lineAxeThree,
+      lineStroke,
+      lineStrokeWidth,
+      midPointStroke,
+      midPointStrokeWidth,
+      midPointShadowBlur,
+      midPointRadius,
+      nodeA,
+      pointSize,
+      shapeStroke,
+      triangleA,
+      triangleB,
+      triangleOpacity,
+      triangleStroke,
+      triangleShadowBlur,
+      triangleStrokeWidth,
+      width,
+    } = this.state;
+
+    const { showPoints, nodeB, toggleLine } = this.props;
+
+    return (
+      <div className="triangle-content">
+        { toggleLine ? (
+          <Stage width={width} height={height}>
+            <MidPoint
+              circlePoints={circlePoints}
+              color={color}
+              shadowBlur={midPointShadowBlur}
+              stroke={midPointStroke}
+              strokeWidth={midPointStrokeWidth}
+              radius={midPointRadius}
+            />
+            <Axes
+              axePointsOne={axePointsOne}
+              axePointsTwo={axePointsTwo}
+              axePointsThree={axePointsThree}
+              stroke={axeStroke}
+              strokeWidth={axeStrokeWidth}
+            />
+          </Stage>
+        )
+          : (
+            <Stage width={width} height={height}>
+              <Liner
+                color={color}
+                handleDragMove={this.handleDragMove}
+                handleDragMoveOne={this.handleDragMoveOne}
+                lineAxeOne={lineAxeOne}
+                lineAxeTwo={lineAxeTwo}
+                lineAxeThree={lineAxeThree}
+                linePoints={linePoints}
+                lineStroke={lineStroke}
+                radius={circleRadius}
+                shadowBlur={triangleShadowBlur}
+                strokeWidth={lineStrokeWidth}
+              />
+            </Stage>
+          )
+        }
+        { showPoints ? (
+          <Stage width={width} height={height}>
+            <Points
+              color={color}
+              fontSize={pointSize}
+              node={nodeA}
+              points={
+                [
+                  { x: triangleA[0].x, y: triangleA[0].y },
+                  { x: triangleA[1].x, y: triangleA[1].y },
+                  { x: triangleA[2].x, y: triangleA[2].y },
+                ]
+              }
+            />
+            <Points
+              color={color}
+              fontSize={pointSize}
+              node={nodeB}
+              points={
+                [
+                  { x: triangleB[0].x, y: triangleB[0].y },
+                  { x: triangleB[1].x, y: triangleB[1].y },
+                  { x: triangleB[2].x, y: triangleB[2].y },
+                ]
+              }
+            />
+          </Stage>
+        ) : ''
+        }
         <Stage width={width} height={height}>
-          <Liner
+          <Triangle
             color={color}
-            handleDragMove={handleDragMove}
-            handleDragMoveOne={handleDragMoveOne}
-            lineAxeOne={lineAxeOne}
-            lineAxeTwo={lineAxeTwo}
-            lineAxeThree={lineAxeThree}
-            linePoints={linePoints}
-            lineStroke={lineStroke}
+            nodeA={nodeA}
+            opacity={triangleOpacity}
+            points={
+              [
+                { x: triangleA[0].x, y: triangleA[0].y },
+                { x: triangleA[1].x, y: triangleA[1].y },
+                { x: triangleA[2].x, y: triangleA[2].y },
+              ]
+            }
             radius={circleRadius}
             shadowBlur={triangleShadowBlur}
-            strokeWidth={lineStrokeWidth}
+            shapeStroke={shapeStroke}
+            stroke={triangleStroke}
+            strokeWidth={triangleStrokeWidth}
+          />
+
+          <Triangle
+            color={color}
+            linePoints={linePoints}
+            nodeB={nodeB}
+            opacity={triangleOpacity}
+            points={
+              [
+                { x: triangleB[0].x, y: triangleB[0].y },
+                { x: triangleB[1].x, y: triangleB[1].y },
+                { x: triangleB[2].x, y: triangleB[2].y },
+              ]
+            }
+            radius={circleRadius}
+            shadowBlur={triangleShadowBlur}
+            shapeStroke={shapeStroke}
+            stroke={triangleStroke}
+            strokeWidth={triangleStrokeWidth}
           />
         </Stage>
-      )
-    }
-    { showPoints ? (
-      <Stage width={width} height={height}>
-        <Points
-          color={color}
-          fontSize={pointSize}
-          node={nodeA}
-          points={
-            [
-              { x: triangleA[0].x, y: triangleA[0].y },
-              { x: triangleA[1].x, y: triangleA[1].y },
-              { x: triangleA[2].x, y: triangleA[2].y },
-            ]
-          }
-        />
-        <Points
-          color={color}
-          fontSize={pointSize}
-          node={nodeB}
-          points={
-            [
-              { x: triangleB[0].x, y: triangleB[0].y },
-              { x: triangleB[1].x, y: triangleB[1].y },
-              { x: triangleB[2].x, y: triangleB[2].y },
-            ]
-          }
-        />
-      </Stage>
-    ) : ''
-    }
-    <Stage width={width} height={height}>
-      <Triangle
-        color={color}
-        nodeA={nodeA}
-        opacity={triangleOpacity}
-        points={
-          [
-            { x: triangleA[0].x, y: triangleA[0].y },
-            { x: triangleA[1].x, y: triangleA[1].y },
-            { x: triangleA[2].x, y: triangleA[2].y },
-          ]
-        }
-        radius={circleRadius}
-        shadowBlur={triangleShadowBlur}
-        shapeStroke={shapeStroke}
-        stroke={triangleStroke}
-        strokeWidth={triangleStrokeWidth}
-      />
+      </div>
+    );
+  }
+}
 
-      <Triangle
-        color={color}
-        linePoints={linePoints}
-        nodeB={nodeB}
-        opacity={triangleOpacity}
-        points={
-          [
-            { x: triangleB[0].x, y: triangleB[0].y },
-            { x: triangleB[1].x, y: triangleB[1].y },
-            { x: triangleB[2].x, y: triangleB[2].y },
-          ]
-        }
-        radius={circleRadius}
-        shadowBlur={triangleShadowBlur}
-        shapeStroke={shapeStroke}
-        stroke={triangleStroke}
-        strokeWidth={triangleStrokeWidth}
-      />
-    </Stage>
-  </div>
-);
-
-TriangleView.propTypes = {
-  height: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
-  circleRadius: PropTypes.number.isRequired,
-  triangleOpacity: PropTypes.number.isRequired,
-  triangleStrokeWidth: PropTypes.number.isRequired,
-  triangleShadowBlur: PropTypes.number.isRequired,
-  lineStrokeWidth: PropTypes.number.isRequired,
-  triangleStroke: PropTypes.string.isRequired,
-  midPointStroke: PropTypes.string.isRequired,
-  midPointStrokeWidth: PropTypes.number.isRequired,
-  midPointShadowBlur: PropTypes.number.isRequired,
-  midPointRadius: PropTypes.number.isRequired,
-  axeStroke: PropTypes.string.isRequired,
-  axeStrokeWidth: PropTypes.number.isRequired,
-  pointSize: PropTypes.number.isRequired,
-  lineStroke: PropTypes.string.isRequired,
-  handleDragMove: PropTypes.func.isRequired,
-  handleDragMoveOne: PropTypes.func.isRequired,
-  color: PropTypes.string.isRequired,
-  shapeStroke: PropTypes.string.isRequired,
-  circlePoints: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  axePointsOne: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  axePointsTwo: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  axePointsThree: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  linePoints: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  lineAxeOne: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  lineAxeTwo: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  lineAxeThree: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  toggleLine: PropTypes.bool.isRequired,
-  showPoints: PropTypes.bool.isRequired,
-  nodeA: PropTypes.shape({
-    A: PropTypes.string.isRequired,
-    B: PropTypes.string.isRequired,
-    C: PropTypes.string.isRequired,
-  }).isRequired,
-  nodeB: PropTypes.shape({
-    A: PropTypes.string.isRequired,
-    B: PropTypes.string.isRequired,
-    C: PropTypes.string.isRequired,
-  }).isRequired,
-  triangleA: PropTypes.arrayOf(PropTypes.shape({
-    x: PropTypes.number.isRequired,
-    y: PropTypes.number.isRequired,
-  })).isRequired,
-  triangleB: PropTypes.arrayOf(PropTypes.shape({
-    x: PropTypes.number.isRequired,
-    y: PropTypes.number.isRequired,
-  })).isRequired,
-};
 
 export default TriangleView;
