f926ec4715737276f122f4435ad42eae51a10e55
Mashkour <azizmashkour@gmail.com>
2018-12-17 16:40:26 +0100

    feat: create polygon state configs file

---
+++ src/config/PolygonState.js
@@ -0,0 +1,13 @@
+export const PolygonState = {
+  axeStroke: '#0091EA',
+  shadowBlur: 5,
+  middleLinePoint: [
+    500,
+    100,
+    500,
+    800,
+  ],
+  middleLinePointLineStroke: 'rgba(6, 129, 218, .5)',
+};
+
+export default PolygonState;
