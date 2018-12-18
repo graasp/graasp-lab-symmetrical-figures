2713bdec765d2fc374c962258603c1665caad114
Mashkour <azizmashkour@gmail.com>
2018-12-18 16:58:52 +0100

    feat: add sub component to testing view component

---
+++ src/component/description/cases/TableDatas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  InputGroup, Input,
+} from 'reactstrap';
+
+const TableDatas = () => (
+  <td>
+    <InputGroup>
+      <Input type="number" min="0" max="435" />
+    </InputGroup>
+  </td>
+);
+
+export default TableDatas;
