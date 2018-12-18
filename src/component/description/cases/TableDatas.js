d1faac2c7a56867526320a039b6d4ee0fe636deb
Mashkour <azizmashkour@gmail.com>
2018-12-18 18:25:07 +0100

    feat: make sure passing event on point coordinate values changed

--- src/component/description/cases/TableDatas.js
+++ src/component/description/cases/TableDatas.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   InputGroup, Input,
 } from 'reactstrap';
 
-const TableDatas = () => (
+const TableDatas = ({ handleDatas }) => (
   <td>
     <InputGroup>
-      <Input type="number" min="0" max="435" />
+      <Input type="number" min="0" max="435" onChange={e => handleDatas(e)} />
     </InputGroup>
   </td>
 );
 
+TableDatas.propTypes = {
+  handleDatas: PropTypes.func.isRequired,
+};
 export default TableDatas;
