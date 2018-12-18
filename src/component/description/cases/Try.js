f16f1b830f2213217a408957a551ad4a67f6a6ea
Mashkour <azizmashkour@gmail.com>
2018-12-18 18:25:53 +0100

    feat: pass input change handling function to child component

--- src/component/description/cases/Try.js
+++ src/component/description/cases/Try.js
@@ -4,11 +4,15 @@ import { Button, Table } from 'reactstrap';
 import TableDatas from './TableDatas';
 
 const Try = ({
+  handleDatas,
   isPolygonActive,
   isSquareActive,
 }) => (
   <div className="testing-container">
-    <h2>We are tryinh here hehe</h2>
+    <p className="testing-message">
+      Observe attentivement le schéma puis complète les cases vides par
+      les coordonnées de chaque point
+    </p>
     <Table borderless>
       <thead>
         <tr>
@@ -28,32 +32,32 @@ const Try = ({
       <tbody>
         <tr>
           <th scope="row">A</th>
-          <TableDatas index="0" name="X" />
-          <TableDatas index="1" name="Y" />
+          <TableDatas index="0" name="X" handleDatas={handleDatas} />
+          <TableDatas index="1" name="Y" handleDatas={handleDatas} />
         </tr>
         <tr>
           <th scope="row">B</th>
-          <TableDatas index="1" name="X" />
-          <TableDatas index="2" name="Y" />
+          <TableDatas index="1" name="X" handleDatas={handleDatas} />
+          <TableDatas index="2" name="Y" handleDatas={handleDatas} />
         </tr>
         <tr>
           <th scope="row">C</th>
-          <TableDatas index="1" name="X" />
-          <TableDatas index="2" name="Y" />
+          <TableDatas index="1" name="X" handleDatas={handleDatas} />
+          <TableDatas index="2" name="Y" handleDatas={handleDatas} />
         </tr>
         { isSquareActive || isPolygonActive ? (
           <tr>
             <th scope="row">D</th>
-            <TableDatas index="1" name="X" />
-            <TableDatas index="2" name="Y" />
+            <TableDatas index="1" name="X" handleDatas={handleDatas} />
+            <TableDatas index="2" name="Y" handleDatas={handleDatas} />
           </tr>
         ) : ''
         }
         { isPolygonActive ? (
           <tr>
             <th scope="row">E</th>
-            <TableDatas index="1" name="X" />
-            <TableDatas index="2" name="Y" />
+            <TableDatas index="1" name="X" handleDatas={handleDatas} />
+            <TableDatas index="2" name="Y" handleDatas={handleDatas} />
           </tr>
         ) : ''
         }
@@ -68,6 +72,7 @@ const Try = ({
 );
 
 Try.propTypes = {
+  handleDatas: PropTypes.func.isRequired,
   isPolygonActive: PropTypes.bool.isRequired,
   isSquareActive: PropTypes.bool.isRequired,
 };
