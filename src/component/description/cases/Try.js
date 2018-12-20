a51f3faf9765064d7737f862696f9b7414f2a86d
Mashkour <azizmashkour@gmail.com>
2018-12-20 11:29:14 +0100

    feat: add inputs to record student test values

--- src/component/description/cases/Try.js
+++ src/component/description/cases/Try.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Table } from 'reactstrap';
+import { Button, Input, Table } from 'reactstrap';
 import TableDatas from './TableDatas';
+import './Try.css';
 
 const Try = ({
   handleDatas,
   handleVerify,
   isPolygonActive,
   isSquareActive,
+  toggleLine,
 }) => (
   <div className="testing-container">
     <p className="testing-message">
@@ -74,6 +76,92 @@ const Try = ({
         }
       </tbody>
     </Table>
+    <p className="testing-message">
+      <b>2-</b>
+      &nbsp;
+      Le point
+      &nbsp;
+      <b>C&apos; : ( 9;8 )</b>
+      &nbsp;
+      est le
+      &nbsp;
+      <b>symétrique</b>
+      &nbsp;
+      de
+      &nbsp;
+      { toggleLine ? (
+        <b>C</b>
+      ) : (
+        <b>B</b>
+      )
+      }
+      &nbsp;
+      par rapport &nbsp;
+      { toggleLine ? (
+        <b>au point M</b>
+      ) : (
+        <b>à la droite</b>
+      )
+      }
+      .
+    </p>
+    <p>
+      Complète donc les points de symétrie:
+    </p>
+    <p>
+      <b>B&apos; : ( 7;6 )</b>
+      &nbsp;
+      est le
+      &nbsp;
+      <b>symétrique</b>
+      &nbsp;
+      de
+      &nbsp;
+      <span className="handle-result">
+        <Input
+          type="text"
+          className="change-value"
+        />
+      </span>
+    </p>
+    <p>
+      <b>A&apos; : ( 10;5 )</b>
+      &nbsp;
+      est le
+      &nbsp;
+      <b>symétrique</b>
+      &nbsp;
+      de
+      &nbsp;
+      <span className="handle-result">
+        <Input
+          type="text"
+          className="change-value"
+        />
+      </span>
+    </p>
+
+    <p>
+      Le triangle&nbsp;
+      <b>ABC</b>
+      &nbsp;et le triangle&nbsp;
+      <b>A&apos;B&apos;C&apos;</b>
+      &nbsp;sont&nbsp;
+      <span className="check-symetric-word">
+        <Input
+          type="text"
+          className="change-value"
+        />
+      </span>
+      &nbsp;par rapport &nbsp;
+      { toggleLine ? (
+        <b>au point M</b>
+      ) : (
+        <b>à la droite</b>
+      )
+      }
+      .
+    </p>
     <Button
       color="secondary"
       onClick={handleVerify}
@@ -88,5 +176,6 @@ Try.propTypes = {
   handleVerify: PropTypes.func.isRequired,
   isPolygonActive: PropTypes.bool.isRequired,
   isSquareActive: PropTypes.bool.isRequired,
+  toggleLine: PropTypes.bool.isRequired,
 };
 export default Try;
