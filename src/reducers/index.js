c80f16d908da69fe89739b437a839dbdd068a782
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2019-01-07 16:24:42 +0100

    feat: import polygon, square and triangle reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import Polygon from './polygon';
+import Square from './square';
+import Triangle from './triangle';
+
+export default combineReducers({
+  Polygon,
+  Square,
+  Triangle,
+});
