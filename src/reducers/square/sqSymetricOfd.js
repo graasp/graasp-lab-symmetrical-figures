4f4309f83d3d3863e52408e2fa476d09cf53ebc7
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2019-01-07 15:23:28 +0100

    feat: create, config and import square practice reducers

---
+++ src/reducers/square/sqSymetricOfd.js
@@ -0,0 +1,33 @@
+import Immutable from 'immutable';
+import {
+  SQ_SYM_OF_D,
+  SQ_WORD_FOUND,
+  SQ_WORD_NOT_FOUND,
+} from '../../types';
+
+const INITIAL_STATE = new Immutable.Map({
+  isSqSymOfBFound: false,
+});
+
+export default (state = INITIAL_STATE, { type }) => {
+  switch (type) {
+    case SQ_WORD_NOT_FOUND:
+      return {
+        ...state,
+        isSqSymOfBFound: false,
+      };
+
+    case SQ_WORD_FOUND:
+      return {
+        ...state,
+        isSqSymOfBFound: true,
+      };
+    case SQ_SYM_OF_D:
+      return {
+        ...state,
+        isSqSymOfBFound: true,
+      };
+    default:
+      return state;
+  }
+};
