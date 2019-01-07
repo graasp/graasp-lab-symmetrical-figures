008d84f85e2d02a7a9a53eef484bfa0a89658999
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2019-01-07 16:21:06 +0100

    feat: create, config and export square practice action types

---
+++ src/types/square/sqSymetricOfb.js
@@ -0,0 +1,5 @@
+export const SQ_SYM_OF_B = 'B';
+export const SQ_SYM_OF_C = 'C';
+export const SQ_SYM_OF_D = 'D';
+export const SQ_WORD_FOUND = 'SQ_WORD_FOUND';
+export const SQ_WORD_NOT_FOUND = 'SQ_WORD_NOT_FOUND';
