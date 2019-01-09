f6a2f27474ae0c308c8aa96ecfd1e3adb1e8f16a
Mashkour <azizmashkour@gmail.com>
2019-01-09 14:20:16 +0100

    feat: create and config default language reducers

--- src/reducers/Setting.js
+++ src/reducers/Setting.js
@@ -1,13 +1,20 @@
 import {
+  DEFAULT_LANGUAGE,
   HEADER_BACKGROUND_COLOR,
 } from '../types';
 
 const INITIAL_STATE = {
+  defaultLanguage: 'en',
   headerBackgroundColor: '#2196F5',
 };
 
 export default (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
+    case DEFAULT_LANGUAGE:
+      return {
+        ...state,
+        defaultLanguage: payload,
+      };
     case HEADER_BACKGROUND_COLOR:
       return {
         ...state,
