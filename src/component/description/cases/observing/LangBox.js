f14f61dcd36130e7b0466b757ddb436503052d28
Mashkour <azizmashkour@gmail.com>
2019-01-09 14:27:43 +0100

    feat: create setting language control component

---
+++ src/component/description/cases/observing/LangBox.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withNamespaces } from 'react-i18next';
+import { Col, Row } from 'reactstrap';
+import Select from 'react-select';
+import { options } from '../../../../config/options';
+
+const LangBox = ({
+  handleLang,
+}) => (
+  <div className="control-container">
+    <Row className="modal-lang-container">
+      <Col xs="4">
+        <h4 className="modal-lang-title">Language</h4>
+      </Col>
+      <Col xs="8">
+        <Select
+          defaultValue={options[0]}
+          options={options}
+          onChange={handleLang}
+          className="App-Select"
+        />
+      </Col>
+    </Row>
+  </div>
+);
+
+LangBox.propTypes = {
+  handleLang: PropTypes.func.isRequired,
+};
+
+export default withNamespaces('translations')(LangBox);
