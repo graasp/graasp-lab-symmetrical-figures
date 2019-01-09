876ec7b7b630b2f38e38b7ca759e03f26c974d20
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2019-01-09 10:47:31 +0100

    fix: separate setting reducer from triangle one

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import Polygon from './polygon';
 import Square from './square';
+import Setting from './Setting';
 import Triangle from './triangle';
 
 export default combineReducers({
   Polygon,
   Square,
+  Setting,
   Triangle,
 });
