4709c392f5d7fbeea7a74306c6664a60fc2b21b3
Mashkour <azizmashkour@gmail.com>
2019-01-17 15:14:39 +0100

    feat: separate setting config icon into different component

---
+++ src/component/description/cases/observing/SettingIcon.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Settings';
+import Fab from '@material-ui/core/Fab';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  fab: {
+    right: theme.spacing.unit * 4,
+    bottom: theme.spacing.unit * 4,
+    position: 'fixed',
+  },
+});
+
+const SettingIcon = ({
+  onOpenModal,
+  classes,
+  themeColor,
+}) => (
+  <div className="modal-container">
+    <Fab
+      color="primary"
+      aria-label="Add"
+      onClick={onOpenModal}
+      className={classes.fab}
+      style={{ backgroundColor: themeColor }}
+    >
+      <AddIcon />
+    </Fab>
+  </div>
+);
+
+SettingIcon.propTypes = {
+  onOpenModal: PropTypes.func.isRequired,
+  themeColor: PropTypes.string.isRequired,
+  classes: PropTypes.shape({}).isRequired,
+};
+const mapStateToProps = state => ({
+  themeColor: state.Setting.themeColor,
+});
+
+const connectedComponent = connect(mapStateToProps)(SettingIcon);
+export default withStyles(styles)(connectedComponent);
