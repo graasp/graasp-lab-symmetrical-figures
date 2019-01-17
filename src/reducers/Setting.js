bce3170b8478aa2c4567ac9a0ab3832ed782b928
Mashkour <azizmashkour@gmail.com>
2019-01-17 16:22:37 +0100

    fix: update redux state based on action type

--- src/reducers/Setting.js
+++ src/reducers/Setting.js
@@ -3,16 +3,19 @@ import {
   SET_LANGUAGE,
   SET_HEADER_BACKGROUND_COLOR,
   SET_TITLE_STATE,
+  SET_GRID_STATE,
+  SET_POINT_STATE,
 } from '../types';
 
 const INITIAL_STATE = {
   changedLanguage: 'en',
   themeColor: '#2196F5',
   showTitle: true,
+  showPoints: true,
+  showGrid: true,
 };
 
 export default (state = INITIAL_STATE, { type, payload }) => {
-  console.log('Getting new language', payload);
   switch (type) {
     case SET_LANGUAGE:
       return {
@@ -28,6 +31,16 @@ export default (state = INITIAL_STATE, { type, payload }) => {
       return {
         ...state,
         showTitle: payload,
+      };
+    case SET_GRID_STATE:
+      return {
+        ...state,
+        showGrid: payload,
+      };
+    case SET_POINT_STATE:
+      return {
+        ...state,
+        showPoints: payload,
       };
     case SET_CONFIG:
       return {
