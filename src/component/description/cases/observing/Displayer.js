a0d1298bf9c90ff105f09268ffa3c19531abe842
Mashkour <azizmashkour@gmail.com>
2019-01-17 16:23:56 +0100

    feat: add grid and points config component to modal

--- src/component/description/cases/observing/Displayer.js
+++ src/component/description/cases/observing/Displayer.js
@@ -22,7 +22,15 @@ const styles = theme => ({
   },
 });
 
-const Displayer = ({ t, toggleTitle, showTitle }) => (
+const Displayer = ({
+  t,
+  toggleTitle,
+  showTitle,
+  handleCheck,
+  handlePointsDisplay,
+  showPoints,
+  showGrid,
+}) => (
   <div className="modal-container">
     <Row className="title-switch">
       <Col xs={8}>
@@ -36,6 +44,30 @@ const Displayer = ({ t, toggleTitle, showTitle }) => (
         />
       </Col>
     </Row>
+    <Row>
+      <Col xs={8}>
+        <h5 className="display-grid">{t('Display points')}</h5>
+      </Col>
+      <Col xs={4}>
+        <Switch
+          onChange={handlePointsDisplay}
+          checked={showPoints}
+          id="point-switch"
+        />
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={8}>
+        <h5 className="display-line">{t('Display grid')}</h5>
+      </Col>
+      <Col xs={4}>
+        <Switch
+          onChange={handleCheck}
+          checked={showGrid}
+          id="grid-switch"
+        />
+      </Col>
+    </Row>
   </div>
 );
 
@@ -44,10 +76,16 @@ Displayer.propTypes = {
   toggleTitle: PropTypes.func.isRequired,
   showTitle: PropTypes.bool.isRequired,
   classes: PropTypes.shape({}).isRequired,
+  handleCheck: PropTypes.func.isRequired,
+  handlePointsDisplay: PropTypes.func.isRequired,
+  showPoints: PropTypes.bool.isRequired,
+  showGrid: PropTypes.bool.isRequired,
 };
 const mapStateToProps = state => ({
   themeColor: state.Setting.themeColor,
   showTitle: state.Setting.showTitle,
+  showPoints: state.Setting.showPoints,
+  showGrid: state.Setting.showGrid,
 });
 
 const connectedComponent = connect(mapStateToProps)(Displayer);
