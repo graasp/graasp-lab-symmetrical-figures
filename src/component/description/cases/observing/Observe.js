23cb1a0a78e3c16a256e80bda932d4d6fe20c22a
Mashkour <azizmashkour@gmail.com>
2019-01-17 16:26:22 +0100

    fix: remove all unused props and components

--- src/component/description/cases/observing/Observe.js
+++ src/component/description/cases/observing/Observe.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {
-  Row,
-  Col,
-} from 'reactstrap';
-import Switch from 'react-switch';
 import Forms from '../../../forms/Forms';
 import RadioBtn from './RadioBtn';
 import { AppState } from '../../../../config/AppState';
@@ -16,10 +11,6 @@ export class Observe extends Component {
   render() {
     const {
       handleForm,
-      handleCheck,
-      showGrid,
-      showPoints,
-      handlePointsDisplay,
       handleView,
       isTriangleActive,
       toggleLine,
@@ -38,30 +29,6 @@ export class Observe extends Component {
           toggleLine={toggleLine}
           t={t}
         />
-        <Row>
-          <Col xs="6">
-            <span className="display-grid">{t('Display points')}</span>
-          </Col>
-          <Col xs="6">
-            <Switch
-              onChange={handlePointsDisplay}
-              checked={showPoints}
-              id="point-switch"
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs="6">
-            <span className="display-line">{t('Display grid')}</span>
-          </Col>
-          <Col xs="6">
-            <Switch
-              onChange={handleCheck}
-              checked={showGrid}
-              id="grid-switch"
-            />
-          </Col>
-        </Row>
         <div className="short-description">
           <h2>{t('Description')}</h2>
           <p className="description-title">
@@ -80,13 +47,9 @@ const mapStateToProps = state => ({
 
 Observe.propTypes = {
   handleForm: PropTypes.func.isRequired,
-  handleCheck: PropTypes.func.isRequired,
-  handlePointsDisplay: PropTypes.func.isRequired,
   handleView: PropTypes.func.isRequired,
   isTriangleActive: PropTypes.bool.isRequired,
   toggleLine: PropTypes.bool.isRequired,
-  showGrid: PropTypes.bool.isRequired,
-  showPoints: PropTypes.bool.isRequired,
   t: PropTypes.func.isRequired,
 };
 
