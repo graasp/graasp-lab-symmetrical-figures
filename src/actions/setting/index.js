9a62b7cc25bacc8778cb7258e928d2a168743b20
azizmashkour <azizmashkour@gmail.com>
2019-02-01 13:45:48 +0100

    fix: remove all unused action creator

--- src/actions/setting/index.js
+++ src/actions/setting/index.js
@@ -1,7 +1,1 @@
 export * from './DefaultLanguage';
-export * from './ThemeColor';
-export * from './SetConfig';
-export * from './TitleState';
-export * from './GridState';
-export * from './PointState';
-export * from './SideMenuState';
