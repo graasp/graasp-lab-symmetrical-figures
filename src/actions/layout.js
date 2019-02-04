f84fe1fa1e48ed510f987aaf8eb20e8c6a020146
azizmashkour <azizmashkour@gmail.com>
2019-02-04 10:41:45 +0100

    feat: dispatch layout configs

--- src/actions/layout.js
+++ src/actions/layout.js
@@ -3,6 +3,8 @@ import {
   TOGGLE_HEADER,
   CHANGE_THEME_COLOR,
 } from '../types';
+import { patchAppInstance } from './appInstance';
+import { getSettings } from './common';
 
 export const toggleSideMenu = payload => (dispatch) => {
   dispatch({
@@ -11,19 +13,34 @@ export const toggleSideMenu = payload => (dispatch) => {
   });
 };
 
-export const toggleHeader = showHeader => (dispatch) => {
+export const toggleHeader = showHeader => (dispatch, getState) => {
   dispatch({
     type: TOGGLE_HEADER,
     payload: showHeader,
   });
+
+  const currentSettings = getSettings(getState);
+  const newSettings = {
+    ...currentSettings,
+    showHeader,
+  };
+  dispatch(patchAppInstance({ data: newSettings }));
 };
 
-export const changeThemeColor = themeColor => (dispatch) => {
+export const changeThemeColor = themeColor => (dispatch, getState) => {
   dispatch({
     type: CHANGE_THEME_COLOR,
     payload: themeColor,
   });
+
+  const currentSettings = getSettings(getState);
+  const newSettings = {
+    ...currentSettings,
+    themeColor,
+  };
+  dispatch(patchAppInstance({ data: newSettings }));
 };
+
 
 export default {
   toggleSideMenu,
