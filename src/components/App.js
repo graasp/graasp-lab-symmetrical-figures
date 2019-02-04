fc0f17b6a9e20c5483c4c5f98f344e0037bbe181
azizmashkour <azizmashkour@gmail.com>
2019-02-04 10:36:28 +0100

    fix: refactor app component and dispatch default settings

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { withNamespaces } from 'react-i18next';
 import { connect } from 'react-redux';
-import Qs from 'qs';
+import { DEFAULT_LANG, DEFAULT_MODE } from '../config/settings';
+import { getAppInstance, getContext } from '../actions';
 import TeacherMode from './modes/TeacherMode';
 import StudentMode from './modes/StudentMode';
 import './App.css';
 
 export class App extends Component {
+  static propTypes = {
+    i18n: PropTypes.shape({
+      defaultNS: PropTypes.string,
+    }).isRequired,
+    dispatchGetContext: PropTypes.func.isRequired,
+    dispatchGetAppInstance: PropTypes.func.isRequired,
+    mode: PropTypes.string,
+    lang: PropTypes.string,
+  };
+
+  static defaultProps = {
+    mode: DEFAULT_MODE,
+    lang: DEFAULT_LANG,
+  };
+
   constructor(props) {
     super(props);
-    const {
-      mode = 'default',
-    } = Qs.parse(window.location.search, { ignoreQueryPrefix: true });
-    this.state = { mode };
+
+    props.dispatchGetContext();
+    props.dispatchGetAppInstance();
+  }
+
+  componentDidMount() {
+    const { lang } = this.props;
+    this.handleChangeLang(lang);
+  }
+
+  componentDidUpdate({ lang: prevLang }) {
+    const { lang } = this.props;
+    if (lang !== prevLang) {
+      this.handleChangeLang(lang);
+    }
+  }
+
+  handleChangeLang = (lang) => {
+    const { i18n } = this.props;
+    i18n.changeLanguage(lang);
   }
 
   render() {
-    const { mode } = this.state;
+    const { mode } = this.props;
 
     switch (mode) {
       // show teacher view when in teacher mode
@@ -36,10 +69,17 @@ export class App extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  changedLanguage: state.setting.changedLanguage,
+const mapStateToProps = ({ context }) => ({
+  lang: context.lang,
+  mode: context.mode,
+  appInstance: context.appInstance,
 });
 
-const connectedComponent = connect(mapStateToProps)(App);
+const mapDispatchToProps = {
+  dispatchGetContext: getContext,
+  dispatchGetAppInstance: getAppInstance,
+};
 
-export default withNamespaces('translations')(connectedComponent);
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default withNamespaces()(ConnectedApp);
