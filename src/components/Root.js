f43b36e92557620c27be1e9d254314c823970dd8
azizmashkour <azizmashkour@gmail.com>
2019-02-04 10:32:04 +0100

    fix: refactor main and root components

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { I18nextProvider } from 'react-i18next';
+import i18nConfig from '../config/i18n';
 import App from './App';
 
-const Root = ({ i18n }) => (
-  <I18nextProvider i18n={i18n}>
+const Root = () => (
+  <I18nextProvider i18n={i18nConfig}>
     <App />
   </I18nextProvider>
 );
 
-Root.propTypes = {
-  i18n: PropTypes.shape({
-    defaultNS: PropTypes.string,
-  }).isRequired,
-};
 export default Root;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-import Qs from 'qs';
-import async from './middlewares/async';
+import Root from './components/Root';
+import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import Root from './components/Root';
-import reducers from './reducers';
-import i18n from './config/i18n';
-import registerServiceWorker from './registerServiceWorker';
-import { DEFAULT_LANGUAGE } from './constants/Settings';
 
 const root = document.getElementById('root');
-const initialState = {};
-const renderApp = () => {
-  const store = createStore(
-    reducers,
-    initialState,
-    applyMiddleware(async, reduxThunk),
-  );
+const { store } = configureStore();
 
-  const {
-    lang = DEFAULT_LANGUAGE,
-  } = Qs.parse(window.location.search, { ignoreQueryPrefix: true });
+ReactDOM.render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  root,
+);
 
-  i18n.changeLanguage(lang);
-
-
-  ReactDOM.render(
-    <Provider store={store}>
-      <Root i18n={i18n} />
-    </Provider>,
-    root,
-  );
-};
-
-renderApp();
 registerServiceWorker();
